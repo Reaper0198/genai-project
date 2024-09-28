0a180273672a8bb1dc3687e741c1f18713f4331b
suyash <suyah88666373@gmail.com>
2024-09-28 22:46:25 +0530

    did

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth',authRouter);
-
+//hello from suyash 
 app.use((err,req,res,next)=>{
   const statusCode=err.statusCode||500;
   const message=err.message||'Internal Server Error';
