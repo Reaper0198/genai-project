0125bffe2fce9ad65dc2b209e391425b3a021b14
Dhanraj Singh <sdhanraj300@gmail.com>
2024-10-02 15:00:13 +0530

    final commit??day2

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,9 @@ app.use((err, req, res, next) => {
     message
   })
 })
-
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
